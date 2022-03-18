3ebc5dc3655fb761b8c8c126756a4db231d82d70
Subin <ewaterbin@naver.com>
2022-03-18 22:14:04 +0900

    commit

--- Infinite_scroll/result/script.js
+++ Infinite_scroll/result/script.js
@@ -5,20 +5,20 @@
     return document.querySelector(target)
   }
 
-  let currentPage = 1
-  let total = 10
+  let currentPage = 1 // 처음 로드했을때 가져오는 첫번째 페이지 
   const limit = 10
-  const end = 100
+  let total = 10 //여태까지 불러온 데이터의 갯수 
+  const end = 100 // 데이터의 총 갯수
 
   const $posts = get('.posts')
   const $loader = get('.loader')
 
   const hideLoader = () => {
-    $loader.classList.remove('show')
+    $loader.classList.remove('show') ////classname show 가 remove되면 opacity : 0 
   }
 
   const showLoader = () => {
-    $loader.classList.add('show')
+    $loader.classList.add('show') //classname show 가 붙으면 opacity : 1
   }
 
 //포스트를 하나하나 처리해줄 함수 
@@ -34,7 +34,7 @@
           <div class="body">${post.body}</div>
       `
       //api 에 id, title, body 있었어서 저렇게 해준것 
-      $posts.appendChild($post)
+      $posts.appendChild($post) //위에 코드 작성 후 이걸 안적어주면 말짱도로묵 
     })
   }
 
@@ -50,24 +50,27 @@
   //return 할 response
   const loadPosts = async (page, limit) => {
     showLoader()
+    //비동기처리 
     try {
       const response = await getPosts(page, limit)
-      showPosts(response)
+      showPosts(response) //로딩노출
     } catch (error) {
-      console.error(error.message)
+      console.error(error.message) //에러처리 
     } finally {
-      hideLoader()
+      hideLoader() //마지막에 실행될 아이 - 로딩 사라지게
     }
   }
 
   const handleScroll = () => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement
 
-    if (total === end) {
+    if (total === end) { //10개에서 100개가 되면 실행되지 않게 그냥 아예 100개가 되었을 때 removeEventLister 해줌 
       window.removeEventListener('scroll', handleScroll)
       return
     }
-
+//scrollTop : 스크롤위치 / clientHeight : 보여지는 영역에 대한 값// 둘을 더한 값 = 전체 컨텐츠 길이 
+// 전체 컨텐츠 길이보다 크거나 같으면 데이터를 불러오는 것 
+// 5를 빼는 이유는 간격을 주기 위함. 겹치면 안되기 때문에 5px인 격
     if (scrollTop + clientHeight >= scrollHeight - 5) {
       currentPage++
       total += 10
