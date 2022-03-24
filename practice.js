8e324c889f73b21da02db752a45e7f6332192e14
Subin <ewaterbin@naver.com>
2022-03-24 19:52:36 +0900

    commit

--- practice.js
+++ practice.js
@@ -1,8 +1,11 @@
-let coffee = 360;
-let first = coffee * (2/3);
-let sec = first * (2/3);
+function price(a,b){
 
-  console.log(coffee + first + sec);
+  let result = a * 0.9;
 
-
-  
+  if (b == true){
+    result = result - 1.5;
+  } 
+  return parseFloat(result.toFixed(2));
+}
+console.log(price(10,true));
+console.log(price(70,false));
